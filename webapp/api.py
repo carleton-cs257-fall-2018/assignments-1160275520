c7dd929667c9d4d225f364dbe339ab8473cda7eb
Yuting <suy@carleton.edu>
2018-10-23 23:50:11 -0500

    phase 5 finished

--- webapp/api.py
+++ webapp/api.py
@@ -52,29 +52,7 @@ def set_headers(response):
 def hello():
     return 'Welcome to Musify!'
 
-@app.route('/singers')
-def get_singers():
-    '''
-    Returns the list of singers in the database. A singer resource
-    will be represented by a JSON dictionary with keys 'id' (int),
-    'singer_name' (string), 'hometown' (string), and 'hotness' (float).
-    Returns an empty list if there's any database failure.
-    '''
-    query = 'SELECT * FROM singers'
-    singer_list = []
-    connection = get_connection()
-    if connection is not None:
-        try:
-            for row in get_select_query_results(connection, query):
-                singer = {"id":row[0], "singer_name":row[1], "hotness":row[2], "hometown":row[3]}
-                singer_list.append(singer)
-        except Exception as e:
-            print(e, file=sys.stderr)
-        connection.close()
-
-    return json.dumps(singer_list)
-
-@app.route('/singer/<singer_id>')
+@app.route('/singers/<singer_id>')
 def get_singer_by_id(singer_id):
     '''
     Returns the singer resource for the singer with the specified id.
@@ -98,25 +76,54 @@ def get_singer_by_id(singer_id):
 
     return json.dumps(singer)
 
-@app.route('/singers/<name>')
-def get_singers_by_name(name):
+@app.route('/singers')
+def get_singers():
     '''
-    Returns a list of all the singers with names containing
-    the specified name. The match is case-insensitive.
+    Returns a list of all the singers with optional search query. 
+    The match is case-insensitive.
+
+    *singer_name [optional]: return singers that contains specific query singer_name string
+
+    *hotness [optional]: return singers with hotness larger than the query hotness
+
+    *hometown [optional] - return singers that contains specific query hometown string
+
+    For example: singers?singer_name=emilio&hometown=texas
+                search for singers whose name contains "emilio" 
+                and their hometwon's name contains "texas".
     
     See get_singers above for a description of the
     singer resource representation.
     '''
-    query = '''SELECT *
-               FROM singers
-               WHERE UPPER(name) LIKE '%%' || UPPER(%s) || '%%'
-               ORDER BY name'''
 
     singer_list = []
+
+    query_singer_name = flask.request.args.get('singer_name')
+    query_hotness = flask.request.args.get('hotness')
+    query_hometown = flask.request.args.get('hometown')
+
+    query_string = ""
+
+    if query_singer_name is not None:
+        query_string += "UPPER(name) LIKE '%%' || UPPER('" + str(query_singer_name) + "')|| '%%'"
+    if query_hotness is not None:
+        if query_string!="":
+            query_string += " AND "
+        query_string += "hotness >" + str(query_hotness)
+    if query_hometown is not None:
+        if query_string!="":
+            query_string += " AND "
+        query_string += "UPPER(hometown) LIKE '%%' || UPPER('" + str(query_hometown) + "')|| '%%'"
+
+
+    query = '''SELECT *
+               FROM singers
+               WHERE ''' + query_string
+
     connection = get_connection()
     if connection is not None:
         try:
-            for row in get_select_query_results(connection, query, (name,)):
+            for row in get_select_query_results(connection, query):
                 singer = {"id":row[0], "singer_name":row[1], "hotness":row[2], "hometown":row[3]}
                 singer_list.append(singer)
         except Exception as e:
@@ -126,31 +133,7 @@ def get_singers_by_name(name):
     return json.dumps(singer_list)
 
 
-
-@app.route('/songs')
-def get_songs():
-    '''
-    Returns the list of songs in the database. A song resource
-    will be represented by a JSON dictionary with keys 'id' (int),
-    'song_name' (string), 'hotness' (float), 'album_id' (int), 'genre_id' (int), 
-    'release_year' (int), and 'duration' (float).
-    Returns an empty list if there's any database failure.
-    '''
-    query = 'SELECT * FROM songs'
-    song_list = []
-    connection = get_connection()
-    if connection is not None:
-        try:
-            for row in get_select_query_results(connection, query):
-                song = {"id":row[0], "song_name":row[1], "hotness":row[2],"album_id":row[3], "genre_id":row[4], "release_year":row[5], "duration": row[6]}
-                song_list.append(song)
-        except Exception as e:
-            print(e, file=sys.stderr)
-        connection.close()
-
-    return json.dumps(song_list)
-
-@app.route('/song/<song_id>')
+@app.route('/songs/<song_id>')
 def get_song_by_id(song_id):
     '''
     Returns the song resource for the song with the specified id.
@@ -174,25 +157,84 @@ def get_song_by_id(song_id):
 
     return json.dumps(song)
 
-@app.route('/songs/<name>')
-def get_songs_by_name(name):
+@app.route('/songs')
+def get_songs():
     '''
-    Returns a list of all the songs with names containing
-    the specified name. The match is case-insensitive.
-    
+    Returns a list of all the songs with optional search query. 
+    The match is case-insensitive.
+
+    *song_name[optional]: return songs whose name contains specific query song_name string
+
+    *singer_name[optional]: return songs sung by singers whose name contains specific query singer_name string
+
+    *hotness [optional]: return songs with hotness larger than the query hotness
+
+    *genre [optional]: return songs with specific query genre
+        
+    *year [optional]: return songs with specific query year
+
+    *album [optional]: return songs from albums whose name contains specific query album string
+
+    For example: /songs?genre=jazz&album=love&year=1993 
+                searches for songs whose genre is jazz, 
+                from albums whose name contains "love" and published in 1993. 
+
     See get_songs above for a description of the
     singer resource representation.
     '''
-    query = '''SELECT *
-               FROM songs
-               WHERE UPPER(song_name) LIKE '%%' || UPPER(%s) || '%%'
-               ORDER BY song_name'''
-
     song_list = []
+
+    query_song_name = flask.request.args.get('song_name')
+    query_hotness = flask.request.args.get('hotness')
+    query_year = flask.request.args.get('year')
+    query_album = flask.request.args.get('album')
+    query_genre = flask.request.args.get('genre')
+    query_singer = flask.request.args.get('singer_name')
+
+
+
+
+    query_string = ""
+    from_string = "songs"
+
+    if query_song_name is not None:
+        query_string += "UPPER(song_name) LIKE '%%' || UPPER('" + str(query_song_name) + "')|| '%%'"
+    if query_hotness is not None:
+        if query_string!="":
+            query_string += " AND "
+        query_string += "songs.hotness >" + str(query_hotness)
+    if query_year is not None:
+        if query_string!="":
+            query_string += " AND "
+        query_string += "year =" + str(query_year)
+
+    if query_album is not None:
+        from_string+=", albums"
+        if query_string!="":
+            query_string += " AND "
+        query_string += "songs.album_id=albums.id AND UPPER(albums.name) LIKE '%%' || UPPER('" + str(query_album) + "')|| '%%'" 
+
+    if query_genre is not None:
+        from_string+=", genres"
+        if query_string!="":
+            query_string += " AND "
+        query_string += "songs.genre_id=genres.id AND UPPER(genres.name) LIKE '%%' || UPPER('" + str(query_genre) + "')|| '%%'" 
+
+    if query_singer is not None:
+        from_string+=", singers, songs_singers"
+        if query_string!="":
+            query_string += " AND "
+        query_string += "singers.id=songs_singers.singer_id AND songs_singers.song_id=songs.id AND UPPER(singers.name) LIKE '%%' || UPPER('" + str(query_singer) + "')|| '%%'" 
+
+
+    query = '''SELECT *
+               FROM ''' + from_string + ''' WHERE ''' + query_string
+
+
     connection = get_connection()
     if connection is not None:
         try:
-            for row in get_select_query_results(connection, query, (name,)):
+            for row in get_select_query_results(connection, query):
                 song = {"id":row[0], "song_name":row[1], "hotness":row[2],"album_id":row[3], "genre_id":row[4], "release_year":row[5], "duration": row[6]}
                 song_list.append(song)
         except Exception as e:
