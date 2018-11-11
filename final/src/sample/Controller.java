7f7063d5d38d347e25d44cc98aad7a528ed60993
1160275520 <wangy3@carleton.edu>
2018-11-11 13:06:44 -0600

    finish other models

--- final/src/sample/Controller.java
+++ final/src/sample/Controller.java
@@ -7,4 +7,43 @@ import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
 
 public class Controller {
+    @FXML private Label scoreLabel;
+    @FXML private Label messageLabel;
+    @FXML private AnimalCrushView AnimalCrushView;
+    private GameboardModel gameboardModel;
+
+    public Controller() {
+    }
+
+    public void initialize() {
+        this.gameboardModel = new GameboardModel(this.AnimalCrushView.getRowCount(), this.AnimalCrushView.getColumnCount());
+        this.update();
+    }
+
+    public double getBoardWidth() {
+        return AnimalCrushView.CELL_WIDTH * this.AnimalCrushView.getColumnCount();
+    }
+
+    public double getBoardHeight() {
+        return AnimalCrushView.CELL_WIDTH * this.AnimalCrushView.getRowCount();
+    }
+
+    private void update() {
+        this.AnimalCrushView.update(this.gameboardModel);
+        this.scoreLabel.setText(String.format("Score: %d", this.gameboardModel.getScore()));
+        if (this.gameboardModel.isGameOver()) {
+            this.messageLabel.setText("Congratulation!You win the game");
+        }
+        else {
+            this.gameboardModel.update();
+        }
+    }
+
+    /**
+     * this method handle the event when the user click two grids.
+     */
+    public void handle(KeyEvent keyEvent) {
+        boolean keyRecognized = true;
+        KeyCode code = keyEvent.getCode();
+    }
 }
